7e39478e379c020d86221607ef0a6c5906232d69
Melany Valdiviezo <melanyvaldiviezo@gmail.com>
2020-05-06 18:30:35 -0400

    coming along

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Home.css";
+const Home = () => {
+  return (
+    <div className="Home">
+      <h1>This is the home page!</h1>
+    </div>
+  );
+};
+
+export default Home;
